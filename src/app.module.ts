ad87d5e4d2160f85ea353eddd1a5093fd9ae784d
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2021-08-23 15:47:12 -0300

    limpando projeto inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FluxoTrabalhoModule } from './fluxo-trabalho/fluxo-trabalho.module';
 
 @Module({
   imports: [FluxoTrabalhoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
