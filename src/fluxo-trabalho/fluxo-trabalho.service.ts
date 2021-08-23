13df8dbfe36037cbd8cfc6169a850377cfb0c0ef
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2021-08-23 16:05:06 -0300

    implementando bando de dados local de fluxos de trabalho

--- src/fluxo-trabalho/fluxo-trabalho.service.ts
+++ src/fluxo-trabalho/fluxo-trabalho.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { FluxoTrabalho } from './domain/entities/fluxo-trabalho';
 
 @Injectable()
-export class FluxoTrabalhoService {}
+export class FluxoTrabalhoService {
+
+  private fluxos: FluxoTrabalho[]
+  
+  constructor(){
+    this.fluxos = []
+  }
+}
